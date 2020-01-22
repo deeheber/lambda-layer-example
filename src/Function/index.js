f1465af45a2cee9750f1688448490388c67f1f61
danielle <danielle@stackery.io>
2020-01-22 07:16:01 -0800

    Base code for function.

--- src/Function/index.js
+++ src/Function/index.js
@@ -1,6 +1,17 @@
-exports.handler = async (event, context) => {
+// eslint-disable-next-line import/no-absolute-path
+const palindrome = require('/opt/nodejs/palindrome');
+
+exports.handler = async event => {
   // Log the event argument for debugging and for use in local development.
   console.log(JSON.stringify(event, undefined, 2));
 
-  return {};
+  // Can  also  dynamically pass this value in via the  event object
+  const words = 'This string should be reversed';
+  const response = palindrome(words);
+
+  return {
+    statusCode: 200,
+    headers: {},
+    response: JSON.stringify(response)
+  };
 };
